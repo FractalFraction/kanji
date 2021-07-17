e3fe0dfdba9f8b813072fd8ef1970ba00a873e5c
Colin <cln.gardiner@outlook.com>
2021-07-17 13:43:34 +0100

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true,
+    'jasmine': true,
+    'jquery': true,
+  },
+  'extends': [
+    'google',
+  ],
+  'parserOptions': {
+    'ecmaVersion': 12,
+    'sourceType': 'module',
+  },
+  'rules': {
+  },
+};
